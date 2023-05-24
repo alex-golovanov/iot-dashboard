9034945233a38ae7e424d2e34ef157486afa1394
Alexander Golovanov <alex.golovanov@gmail.com>
2023-05-25 02:06:22 +0300

    feat: add css vanilla extract

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
 import path from 'path';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    vanillaExtractPlugin(),
     !process.env.VITEST ? checker({ typescript: true }) : undefined,
   ],
   resolve: {
