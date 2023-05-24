ca7fc8699ff2c31fdba58d718fabb2d77f98cf54
Alexander Golovanov <alex.golovanov@gmail.com>
2023-05-24 23:44:51 +0300

    feat: add sensors filter

--- client/src/widgets/Sensors/Sensors.tsx
+++ client/src/widgets/Sensors/Sensors.tsx
@@ -1,11 +1,27 @@
+import { useMemo, useState } from 'react';
 import { Sensor } from '../Sensor/Sensor';
 import { useSensors } from './useSensors';
 
 export const Sensors = () => {
   const sensors = useSensors();
+  const [showConnected, setShowConnected] = useState(false);
+  const filteredSensors = useMemo(() => {
+    const sensorArray = Object.values(sensors);
+    return showConnected
+      ? sensorArray.filter((sensor) => sensor.connected === true)
+      : sensorArray;
+  }, [sensors, showConnected]);
+
   return (
     <div>
-      {Object.values(sensors).map((sensor) => (
+      <input
+        type='checkbox'
+        checked={showConnected}
+        onChange={() =>
+          setShowConnected((prevShowConnected) => !prevShowConnected)
+        }
+      />
+      {filteredSensors.map((sensor) => (
         <Sensor key={sensor.id} {...sensor} />
       ))}
     </div>
