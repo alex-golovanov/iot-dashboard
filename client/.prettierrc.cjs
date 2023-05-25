764423bc2c6090ce4b53a3047bc9a58dfdf43975
Alexander Golovanov <alex.golovanov@gmail.com>
2023-05-26 00:30:16 +0300

    style: add prettier config

---
+++ client/.prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  printWidth: 90,
+  trailingComma: 'all',
+  proseWrap: 'never',
+};
